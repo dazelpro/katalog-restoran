6f7a0da822ca94ed844db54364ca071a2698b371
dazelpro <dazelpro@gmail.com>
2020-08-21 16:47:35 +0700

    add budged

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,6 +10,7 @@ import ('../DATA.json').then(({default: jsonData}) => {
         dataList +=`
         <div class="list_item">
             <img class="list_item_thumb" src="${data['pictureId']}" alt="${data['name']}" title="${data['name']}">
+            <div class="city">${data['city']}</div>
             <div class="list_item_content">
                 <p class="list_item_rating">
                     Rating : 
