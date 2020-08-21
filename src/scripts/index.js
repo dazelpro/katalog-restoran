6b65e5741280a78be3079d6cae85985787b583f6
dazelpro <dazelpro@gmail.com>
2020-08-21 14:56:03 +0700

    get data json

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,27 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
-
-console.log('Hello Coders! :)');
+import ('../DATA.json').then(({default: jsonData}) => {
+    console.log(jsonData)
+    let datas = jsonData['restaurants']
+    let dataList = '';
+    datas.forEach(function(data) {
+        dataList +=`
+        <div class="list_item">
+            <img class="list_item_thumb" src="${data['pictureId']}" alt="${data['name']}" title="${data['name']}">
+            <div class="list_item_content">
+                <p class="list_item_rating">
+                    Rating : 
+                    <a href="#" class="list_item_rating_value">${data['rating']}</a>
+                </p>
+                <h1 class="list_item_title"><a href="#">${data['name']}</a></h1>
+                <div class="list_item_desc">${data['description'].slice(0, 150)}...</div>
+            </div>
+        </div>
+        `;
+    });
+    document.querySelector('#tes').innerHTML = dataList;  
+});
 
 const menu = document.querySelector('#menu');
 const hero = document.querySelector('.hero');
