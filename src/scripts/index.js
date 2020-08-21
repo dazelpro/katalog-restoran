8a693b2227d1f50e6c6f44f48291a7a4f629b179
dazelpro <dazelpro@gmail.com>
2020-08-21 09:01:04 +0700

    layout css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,23 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 
 console.log('Hello Coders! :)');
+
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function (event) {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
